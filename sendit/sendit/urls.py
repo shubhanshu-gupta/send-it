f0521b9cae2c38d8971d25d84400c4a422f41bdf
shubhanshu-gupta <shubhanshu2007@gmail.com>
2015-12-31 00:40:23 +0530

    Admin site added

--- sendit/sendit/urls.py
+++ sendit/sendit/urls.py
@@ -20,4 +20,5 @@ from django.contrib import admin
 urlpatterns = [
 	url(r'^senditapp/', include('senditapp.urls'))
     #url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
 ]
