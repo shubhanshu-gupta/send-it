b897b3f39fa63edcffd0387def13c4a7ef720eae
Vaibhavi <ranihaileydesai@gmail.com>
2015-12-31 14:26:50 +0530

    updated view
    added link to the ui folder and imported jinja templating language - here's the documentation of jinja for reference : http://jinja.pocoo.org/docs/dev/

--- sendit/senditapp/views.py
+++ sendit/senditapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from senditapp.models import *
+
+import jinja2
+from jinja2.ext import loopcontrols
+
+jinja_environ = jinja2.Environment(loader=jinja2.FileSystemLoader(['ui']), extensions=[loopcontrols])
 
 def index(request):
 	return HttpResponse("Hello World!")
