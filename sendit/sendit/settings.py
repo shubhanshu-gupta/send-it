709b8f6ac17305ef68209171c04e9149d77f714d
Vaibhavi Desai <Vaibhavi Desai>
2016-01-02 22:16:03 +0530

    autocomplete added with Google Maps

--- sendit/sendit/settings.py
+++ sendit/sendit/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'senditapp'
-    #'location_field'
-    'geoposition'
 ]
 
 GEOPOSITION_MAP_OPTIONS = {
--- sendit/senditapp/models.py
+++ sendit/senditapp/models.py
@@ -3,9 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 from django import forms
 from django.contrib.gis.db import models
-from django.contrib.gis.geos import Point
-#from location_field.models.spatial import LocationField
-from geoposition.fields import GeopositionField
+
+
+
 
 #time_widget = forms.widgets.TimeInput(attrs={'class': 'time-pick'})
 #valid_time_formats = ['%H:%M', '%I:%M%p', '%I:%M %p']
@@ -14,8 +14,6 @@ class Ride(models.Model):
 	#pickup
     pick = models.CharField(max_length=300)
     #location = LocationField(based_fields=[pick], zoom=7, default='Point(1.0 1.0)')
-    #objects = models.GeoManager()
-	position = GeopositionField()
     #drop
     drop = models.CharField(max_length=300)
     #location = LocationField(based_fields=[drop], zoom=7, default='Point(1.0 1.0)')
